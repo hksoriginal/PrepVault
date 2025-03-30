7d3611ead98a91f9536fb312ee165d60d0d1eee1
hksoriginal <harshitkumar454@gmail.com>
2025-03-30 20:50:48 +0530

    fixed layout for NLP

--- pages/3_🗣️_Natural_Language_Processing.py
+++ pages/3_🗣️_Natural_Language_Processing.py
@@ -13,39 +13,40 @@ html_creator = HTMLElementCreator()
 
 st.header("Natural Language Processing ")
 st.markdown("Natural Language Processing (NLP) is a field of artificial intelligence focused on enabling computers to understand, interpret, and generate human language. By combining techniques from linguistics, computer science, and machine learning, NLP allows machines to process text and speech in meaningful ways. Common NLP tasks include text classification, sentiment analysis, machine translation, and question answering. Modern advancements, particularly in deep learning and models like BERT and GPT, have significantly improved the ability of machines to handle complex language tasks, making NLP crucial for applications such as chatbots, language translation, and personal assistants.")
-st.divider()
 
+nlp_tabs = st.tabs(['NLP Concepts', 'BERT', 'Interview Questions'])
 try:
-    st.header("NLP Concepts")
-    with st.columns([0.05, 0.95])[1]:
-        st.markdown("Common NLP Concepts")
-        for index, (title, value) in enumerate(NLP_CONCEPTS_MAPPER.items()):
-            with st.expander(label=title, expanded=False):
-                st.header(title)
-                html_creator.create_youtube_link_button(
-                    label=title, url=value[2])
-                with st.columns([0.7, 0.3])[0]:
-                    if len(value[1]) >= 1:
-                        for image_path in value[1]:
-                            st.image(image=image_path,
-                                     caption=(image_path.split('/')[-1].split(".")[0]).capitalize())
-                st.markdown(value[0])
+    with nlp_tabs[0]:
+        st.header("NLP Concepts")
+        with st.columns([0.05, 0.95])[1]:
+            st.markdown("Common NLP Concepts")
+            for index, (title, value) in enumerate(NLP_CONCEPTS_MAPPER.items()):
+                with st.expander(label=title, expanded=False):
+                    st.header(title)
+                    html_creator.create_youtube_link_button(
+                        label=title, url=value[2])
+                    with st.columns([0.7, 0.3])[0]:
+                        if len(value[1]) >= 1:
+                            for image_path in value[1]:
+                                st.image(image=image_path,
+                                        caption=(image_path.split('/')[-1].split(".")[0]).capitalize())
+                    st.markdown(value[0])
 
     # 1
-    st.header("Bidirectional Encoder Representations from Transformers (BERT)")
-    with st.columns([0.05, 0.95])[1]:
-        st.markdown("BERT (Bidirectional Encoder Representations from Transformers) is a powerful language model that has revolutionized the field of natural language processing (NLP). It's composed of several key components that work together to process and understand text.")
-        for index, (title, value) in enumerate(BERT_MAPPER.items()):
-            with st.expander(label=title, expanded=False):
-                st.header(title)
-                html_creator.create_youtube_link_button(
-                    label=title, url=value[2])
-                with st.columns([0.6, 0.4])[0]:
-                    st.image(image=value[1])
-                st.markdown(value[0], unsafe_allow_html=True)
-    
-    st.divider()
-    with st.expander("Interview Questions"):
+    with nlp_tabs[1]:
+        st.header("Bidirectional Encoder Representations from Transformers (BERT)")
+        with st.columns([0.05, 0.95])[1]:
+            st.markdown("BERT (Bidirectional Encoder Representations from Transformers) is a powerful language model that has revolutionized the field of natural language processing (NLP). It's composed of several key components that work together to process and understand text.")
+            for index, (title, value) in enumerate(BERT_MAPPER.items()):
+                with st.expander(label=title, expanded=False):
+                    st.header(title)
+                    html_creator.create_youtube_link_button(
+                        label=title, url=value[2])
+                    with st.columns([0.6, 0.4])[0]:
+                        st.image(image=value[1])
+                    st.markdown(value[0], unsafe_allow_html=True)
+
+    with nlp_tabs[2]:
         st.image('./Instructions/NLP/nlp_int.png')
         st.markdown(NLP_INTERVIEW_Q)
 
